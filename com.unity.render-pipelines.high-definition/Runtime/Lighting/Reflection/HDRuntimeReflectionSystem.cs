27ea32a6676d197b64f4d564bb9b7d6de85b8806
Frédéric Vauchelles <fredericv@unity3d.com>
2020-01-08 11:54:30 +0100

    wip fix update fo cached data for probes

--- com.unity.render-pipelines.high-definition/Runtime/Lighting/Reflection/HDRuntimeReflectionSystem.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/Reflection/HDRuntimeReflectionSystem.cs
@@ -1,9 +1,21 @@
+using System;
+using System.Reflection;
 using UnityEngine.Experimental.Rendering;
 
 namespace UnityEngine.Rendering.HighDefinition
 {
     class HDRuntimeReflectionSystem : ScriptableRuntimeReflectionSystem
     {
+        static MethodInfo BuiltinUpdate;
+
+        static HDRuntimeReflectionSystem()
+        {
+            var type =
+                Type.GetType("UnityEngine.Experimental.Rendering.BuiltinRuntimeReflectionSystem,UnityEngine");
+            var method = type.GetMethod("BuiltinUpdate", BindingFlags.Static | BindingFlags.NonPublic);
+            BuiltinUpdate = method;
+        }
+
         static HDRuntimeReflectionSystem k_instance = new HDRuntimeReflectionSystem();
 
         // We must use a static constructor and only set the system in the Initialize method
@@ -16,8 +28,14 @@ namespace UnityEngine.Rendering.HighDefinition
                 ScriptableRuntimeReflectionSystemSettings.system = k_instance;
         }
 
-        // Note: method bool TickRealtimeProbes(); in base will create GC.Alloc due to Unity binding code
+        // Note: method bool TickRealtimeProbes() will create GC.Alloc due to Unity binding code
         // (bool as return type is not handled properly)
         // Will be fixed in future release of Unity.
+
+        public override bool TickRealtimeProbes()
+        {
+            BuiltinUpdate.Invoke(null, new object[0]);
+            return base.TickRealtimeProbes();
+        }
     }
 }
